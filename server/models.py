1939cda8b7443bafb2324894b23f8626dd456a29
johney <johneyburke@gmail.com>
2024-08-30 09:12:56 -0600

    all tests passing

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -12,6 +13,7 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     serialize_rules = ('-baked_goods.bakery',)
+
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
